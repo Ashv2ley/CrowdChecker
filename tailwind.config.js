e7b6c30622df666f1a6be6af81e7248b217ac611
ashv2ley <ashley0425@yahoo.com>
2025-05-07 10:27:55 -0700

    added custom color themes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
+    colors: {
+      'dark-red': '#E06666',
+      'red': '#EA9999',
+      'dark-green': '#7ABD7E',
+      'green': '#B6D7A8',
+      'dark-yellow': '#FFC571',
+      'yellow': '#FAD5A0',
+      'gray': '#D7D7D7AB',
+    },
     extend: {},
   },
   plugins: [],
