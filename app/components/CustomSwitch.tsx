882ea00d9ccf9c2a188214c2c5402786ae8f386f
Ashley Anyanwu <ashley0425@yahoo.com>
2025-05-12 21:40:54 -0700

    Working on sign up page

--- app/components/CustomSwitch.tsx
+++ app/components/CustomSwitch.tsx
@@ -8,18 +8,18 @@ export default function CustomSwitch({selectionMode, option1, option2, onSelectS
         onSelectSwitch(value);
     }
     return (
-        <View className={"rounded-full bg-gray flex-row justify-center"}>
+        <View className={"w-full rounded-full bg-gray flex-row justify-center px-2 py-2"}>
             <TouchableOpacity
                 activeOpacity={1}
                 onPress={() => updateSwitchData(1)}
-                className={`flex-1 justify-center items-center rounded-full ${getSelectionMode === 1 ? "bg-dark-red" : "bg-none"} p-5`}
+                className={`flex-1 justify-center items-center rounded-full p-4 ${getSelectionMode === 1 ? "bg-dark-red" : "bg-transparent"}`}
             >
                 <Text className={`font-medium text-black`}>{option1}</Text>
             </TouchableOpacity>
             <TouchableOpacity
                 activeOpacity={1}
                 onPress={() => updateSwitchData(2)}
-                className={`flex-1 justify-center items-center rounded-full ${getSelectionMode === 2 ? "bg-dark-red" : "bg-none"}`}
+                className={`flex-1 justify-center items-center rounded-full p-4 ${getSelectionMode === 2 ? "bg-dark-yellow" : "bg-transparent"}`}
             >
                 <Text className={`font-medium text-black`}>{option2}</Text>
             </TouchableOpacity>
--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,4 +1,4 @@
-import {View, Text, SafeAreaView} from "react-native";
+import {View, Text, SafeAreaView, TextInput} from "react-native";
 import CustomSwitch from "@/app/components/CustomSwitch"
 import {useState} from "react";
 export default function Onboarding() {
@@ -7,19 +7,59 @@ export default function Onboarding() {
         setOnboardingMode(value)
     }
     return (
-        <SafeAreaView className={"p-10"}>
-            <View className={"gap-4 justify-center items-center p-10"}>
-                <Text className={"text-3xl font-bold"}>Get Started Now</Text>
-                <Text>Create an account or log in to explore our app</Text>
+        <SafeAreaView>
+            <View className="gap-12 p-6">
+                <View className="items-center gap-4 mb-2">
+                    <Text className="text-3xl font-bold">Get Started Now</Text>
+                    <Text>Create an account or log in to explore our app</Text>
+                </View>
+                <CustomSwitch
+                    selectionMode={1}
+                    option1={"Sign Up"}
+                    option2={"Log In"}
+                    onSelectSwitch={onSelectSwitch}
+                />
+                <View className="w-full px-4">
+                    {onboardingMode === 1 && (
+                        <View className="flex-col justify-between">
+                            {/* First Name and Last Name */}
+                            <View className="flex-row w-full">
+                                <View className="flex-1 mr-2">
+                                    <Text className="mb-1 font-medium">First Name</Text>
+                                    <TextInput className="border-2 border-gray rounded-full p-4 w-full" />
+                                </View>
+
+                                <View className="flex-1 ml-2">
+                                    <Text className="mb-1 font-medium">Last Name</Text>
+                                    <TextInput className="border-2 border-gray rounded-full p-4 w-full" />
+                                </View>
+                            </View>
+
+                            {/* Email */}
+                            <View>
+                                <Text className="mb-1 font-medium">Email</Text>
+                                <TextInput className="border-2 border-gray rounded-full p-4 w-full" />
+                            </View>
+
+                            {/* Password */}
+                            <View>
+                                <Text className="mb-1 font-medium">Password</Text>
+                                <TextInput className="border-2 border-gray rounded-full p-4 w-full" />
+                            </View>
+
+                            {/* Confirm Password */}
+                            <View>
+                                <Text className="mb-1 font-medium">Confirm Password</Text>
+                                <TextInput className="border-2 border-gray rounded-full p-4 w-full" />
+                            </View>
+                        </View>
+                    )}
+
+                    {onboardingMode === 2 && (
+                        <Text className="text-center font-medium text-lg">Login</Text>
+                    )}
+                </View>
             </View>
-            <CustomSwitch
-                selectionMode={1}
-                option1={"Sign Up"}
-                option2={"Log In"}
-                onSelectSwitch={onSelectSwitch}
-            />
-            {onboardingMode === 1 && (<Text>signup</Text>)}
-            {onboardingMode === 2 && (<Text>login</Text>)}
 
         </SafeAreaView>
     );
