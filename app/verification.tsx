2a6684b3fb30a9cd380febdc3dcdc2f912d20ec7
Ashley Anyanwu <ashley0425@yahoo.com>
2025-05-17 19:27:35 -0700

    verification page ui 80% finished - working on input

--- app/verification.tsx
+++ app/verification.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
+import { useRouter } from "expo-router";
 import { Text, SafeAreaView, View } from 'react-native';
-import { useRouter } from 'expo-router';
 import CustomButton from "@/app/components/CustomButton"
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
@@ -8,24 +8,36 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 export default function Verification() {
     const router = useRouter();
     return (
-        <SafeAreaView>
-            <View className="flex-row justify-between">
-                <MaterialIcons name="arrow-back-ios" size={24} color="black" />
-                <Text className="text-3xl font-bold">Verification</Text>
+        <SafeAreaView className="m-6 gap-12 p-6 items-center">
+            <View className="relative w-full h-12 items-center justify-center">
+                {/* Back Arrow */}
+                <View className="absolute left-0">
+                    <MaterialIcons
+                        name="arrow-back-ios"
+                        size={24}
+                        color="black"
+                        className="bg-gray rounded-full h-10 w-10 p-2"
+                        onPress={() => router.push('/onboarding')}
+                    />
+                </View>
+
+                {/* Centered Title */}
+                <Text className="text-3xl font-bold text-center">Verification</Text>
             </View>
-            <View className="bg-dark-green h-10 w-10">
-                <MaterialCommunityIcons name="message-lock-outline" size={24} color="white" className={"justify-center items-center"}/>
+
+            <View className="bg-green h-48 w-48 rounded-full justify-center items-center m-14">
+                <MaterialCommunityIcons name="message-lock-outline" size={56} color="white" className={"bg-dark-green h-36 w-36 rounded-full p-10"}/>
             </View>
-            <View>
-                <Text>Verification code</Text>
-                <Text>Enter the verification code we’ve sent to your janedoe@gmail.com</Text>
+            <View className="flex-col justify-center items-center gap-5">
+                <Text className={"text-2xl font-bold"}>Verification code</Text>
+                <Text className={"text-lg text-center"}>Enter the verification code we’ve sent to your janedoe@gmail.com</Text>
             </View>
             {/* Verification code input*/}
 
             <CustomButton bgColor={"bg-dark-green"} pageNav={"(tabs)/homepage"} text={"Confirm"}/>
-            <View>
+            <View className="flex-row gap-1">
                 <Text>Didn’t receive the code?</Text>
-                <Text>Resend</Text>
+                <Text className={"font-bold text-dark-green"}>Resend</Text>
             </View>
         </SafeAreaView>
     )
