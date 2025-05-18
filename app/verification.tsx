9aad89695aa3b6118d8c2a381c77db5247920fc2
Ashley Anyanwu <ashley0425@yahoo.com>
2025-05-17 18:48:16 -0700

    working on verification ui

--- app/verification.tsx
+++ app/verification.tsx
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Text, SafeAreaView, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, View } from 'react-native';
 import { useRouter } from 'expo-router';
+import CustomButton from "@/app/components/CustomButton"
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Verification() {
     const router = useRouter();
     return (
         <SafeAreaView>
-            <Text className="text-3xl font-bold">Verification</Text>
-            <TouchableOpacity className={"bg-dark-green"} onPress={() => router.navigate("/(tabs)/homepage")}>
-                <Text>
-                    Homepage button
-                </Text>
-            </TouchableOpacity>
+            <View className="flex-row justify-between">
+                <MaterialIcons name="arrow-back-ios" size={24} color="black" />
+                <Text className="text-3xl font-bold">Verification</Text>
+            </View>
+            <View className="bg-dark-green h-10 w-10">
+                <MaterialCommunityIcons name="message-lock-outline" size={24} color="white" className={"justify-center items-center"}/>
+            </View>
+            <View>
+                <Text>Verification code</Text>
+                <Text>Enter the verification code we’ve sent to your janedoe@gmail.com</Text>
+            </View>
+            {/* Verification code input*/}
+
+            <CustomButton bgColor={"bg-dark-green"} pageNav={"(tabs)/homepage"} text={"Confirm"}/>
+            <View>
+                <Text>Didn’t receive the code?</Text>
+                <Text>Resend</Text>
+            </View>
         </SafeAreaView>
     )
 }
