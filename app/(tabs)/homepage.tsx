8942e165d7630d22916428448eab135e7c18d8ae
Lana Ramadan <lanamramadan@gmail.com>
2025-05-19 17:42:14 -0700

    Displaying home page components

--- app/(tabs)/homepage.tsx
+++ app/(tabs)/homepage.tsx
@@ -1,11 +1,28 @@
-import { View, Text, SafeAreaView } from "react-native";
-
+import { SafeAreaView, ScrollView, View } from "react-native";
+import HomePageSection from "../components/HomePageSection";
 export default function HomePage() {
 
     return (
         <SafeAreaView>
-            <View>
-                <Text>Home</Text>
+            <View className="flex-1 bg-cream pt-10">
+                <ScrollView>
+                    <HomePageSection
+                    title="Gyms"
+                    cards={[{ name: 'Anteater Recreation Center', icon: 'High' },
+                            { name: 'Mesa Court Recreation Center', icon: 'Low' }]}
+                    />
+
+                    <HomePageSection
+                    title="Dining Halls"
+                    cards={[{ name: 'Brandywine Commons', icon: 'Moderate' }, 
+                            { name: 'The Anteatery', icon: 'High' }]}
+                    />
+                    
+                    <HomePageSection 
+                    title="Libraries" 
+                    cards={[{ name: 'Science Library', icon: 'High' },
+                            { name: 'Langson Library', icon: 'High' }]} />
+                </ScrollView>
             </View>
         </SafeAreaView>
     );
