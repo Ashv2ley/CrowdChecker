4642dcdc490cff8a1b84336277b2f28061656b06
Ashley Anyanwu <ashley0425@yahoo.com>
2025-05-21 02:50:52 -0700

    created and implemented header component

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,30 +1,34 @@
 import {Tabs} from "expo-router";
+import { View } from "react-native";
+import Header from "../components/Header"
 
 export default function _Layout() {
-
     return (
-        <Tabs>
-            <Tabs.Screen
-            name="homepage"
-            options={{
-                title: "Home",
-                headerShown: false
-            }}
-            />
-            <Tabs.Screen
-                name="favorites"
-                options={{
-                    title: "Favorites",
-                    headerShown: false
-                }}
-            />
-            <Tabs.Screen
-                name="profile"
-                options={{
-                    title: "Profile",
-                    headerShown: false
-                }}
-            />
-        </Tabs>
+        <View style={{ flex: 1 }}>
+            <Header/>
+            <Tabs>
+                <Tabs.Screen
+                    name="homepage"
+                    options={{
+                        title: "Home",
+                        headerShown: false
+                    }}
+                />
+                <Tabs.Screen
+                    name="favorites"
+                    options={{
+                        title: "Favorites",
+                        headerShown: false
+                    }}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: "Profile",
+                        headerShown: false
+                    }}
+                />
+            </Tabs>
+        </View>
     );
 }
---
+++ app/components/Header.tsx
@@ -0,0 +1,29 @@
+import {useRouter} from "expo-router";
+import {Image, TouchableOpacity, View} from "react-native";
+import React from "react";
+import data from "../../data.json"
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function Header() {
+    const router = useRouter();
+    return (
+        <SafeAreaView>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingHorizontal: 20}}>
+                <TouchableOpacity onPress={() => router.push("/(tabs)/homepage")}>
+                    <Image
+                        source={require('../../assets/images/crowdy-logo.png')}
+                        style={{ width: 50, height: 50 }}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => router.push("/(tabs)/profile")}>
+                    <Image
+                        source={{ uri: data.user.image }}
+                        style={{ width: 50, height: 50, borderRadius: 100 }}
+                        resizeMode="contain"
+                    />
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    );
+}
